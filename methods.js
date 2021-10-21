dac8db6a1ee9a204d4c45f95461d8b1218bbfbc1
Keller Beckstead <kellerjbeckstead@gmail.com>
2021-10-21 06:53:28 -0600

    another node push

--- methods.js
+++ methods.js
@@ -5,10 +5,27 @@ let { people } = require('./data');
 //static assets
 app.use(express.static('./methods-public'))
 
+// parse jsonTha
+app.use(express.json())
+
 //parse form data
 app.use(express.urlencoded({ extended: false}))
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people })
+})
+
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+    console.log(name);
+    if(!name) {
+        return res.status(400).json({ success: false, msg: 'please provide name value'});
+    }
+
+    res.status(201).json({ success: true, person: name })
+})
+
+app.post('/api/postman/people', (req, res) => {
+    
 })
 
 app.post('/login', (req, res) => {
